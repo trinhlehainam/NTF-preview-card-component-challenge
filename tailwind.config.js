994fd5ffd2ceeb72f3dcfc14c6d960c7b2875f65
checker <trinhlehainam@protonmail.com>
2021-12-24 18:16:51 +0900

    Finished design NFT card with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'cyan-custom': 'hsl(178, 100%, 50%)',
+                'custom-bg': 'hsl(217, 54%, 11%)',
+                'card-bg': 'hsl(216, 50%, 16%)',
+                'custom-line': 'hsl(215, 32%, 27%)',
+                'soft-blue': 'hsl(215, 51%, 70%)',
+            },
+        },
+        fontFamily: {
+            'outfit': ['Outfit']
+        }
+    },
+    plugins: [],
+}
